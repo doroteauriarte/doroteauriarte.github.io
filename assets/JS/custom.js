0d1a1be632da587aa09c02badab72c585a4c8ba8
Imanol Jiménez <ijimenez@irontec.com>
2022-04-30 20:56:59 +0200

    fix: slider & margins

--- assets/JS/custom.js
+++ assets/JS/custom.js
@@ -1,5 +1,6 @@
 //_______________________GALERIA DE IMAGENES___________________________
 
+// const Splide = require();
 const placas = [
   "Imagenes/placas/abstraccion.png",
   "Imagenes/placas/africa.png",
@@ -39,11 +40,11 @@ const slides = [
   "Imagenes/cuadros/paisaje_nevado.jpg",
   "Imagenes/cuadros/pajaros.jpg"
 ];
-const slider = document.getElementById('slider');
+const slider = document.getElementsByClassName('splide__list').item(0);
 
 for (let i = 0; i < slides.length; i++) {
-  const slide = document.createElement('div');
-  slide.classList.add('slide');
+  const slide = document.createElement('li');
+  slide.classList.add('splide__slide', 'slide');
   const imgCuadro = document.createElement('img');
   imgCuadro.src = slides[i];
   slide.appendChild(imgCuadro);
@@ -54,51 +55,117 @@ for (let i = 0; i < slides.length; i++) {
   slider.appendChild(slide);
 }
 
-let salida = slides.length - 1;
-let principal = 0;
-let entrada = 1;
-slider.children.item(salida).classList.add('salida');
-slider.children.item(principal).classList.add('principal');
-slider.children.item(entrada).classList.add('entrada');
 
-setInterval(() => {
-  const salidaElm = slider.children.item(salida);
-  salidaElm.ontransitionend = () => {
-    salidaElm.classList.remove('salida', 'saliendo');
-    salidaElm.ontransitionend = undefined;
-  }
-  salidaElm.classList.add('saliendo');
+const splide = new Splide('.splide', {
+  type: 'loop',
+  perPage: 1,
+  autoplay: true,
+  interval: 2000,
+  arrows: false,
+  pagination: false,
+  pauseOnHover: false
+});
+splide.mount();
 
-  salida = principal;
+const imgs = document.querySelectorAll('.splide__slide > img');
+imgs.forEach((img) => {
+  img.addEventListener('mouseenter', () => {
+    if (!splide.Components.Autoplay.isPaused()) {
+      splide.Components.Autoplay.pause();
+    }
+  });
+  img.addEventListener('mouseleave', () => {
+    if (splide.Components.Autoplay.isPaused()) {
+      splide.Components.Autoplay.play();
+    }
+  });
+})
+// let salida = slides.length - 1;
+// let principal = 0;
+// let entrada = 1;
+// slider.children.item(salida).classList.add('salida');
+// slider.children.item(principal).classList.add('principal');
+// slider.children.item(entrada).classList.add('entrada');
+// slider.childNodes.forEach((node, idx) => {
+//   let iterationStart = 0;
+//   switch(idx){
+//     case 0:
+//       iterationStart = (1 / slides.length * 6) / 17;
+//       break;
+//     case 1: 
+//       iterationStart =(1 / slides.length * 1) / 17;
+//       break;
+//     case (slides.length - 1): 
+//       iterationStart = (1 / slides.length * 11) / 17;
+//   }
 
-  const principalElm = slider.children.item(principal);
-  principalElm.ontransitionend = () => {
-    principalElm.classList.add('salida');
-    principalElm.classList.remove('principal-salida', 'principal');
-    principalElm.ontransitionend = undefined;
-  }
-  principalElm.classList.add('principal-salida');
+//   node.animate([
+//     { right: "-50%", transform: "scale(.1)" },
+//     { right: "0", transform: "scale(.4)", offset: (1 / slides.length * 1) / 17 },
+//     { right: "0", transform: "scale(.4)", offset: (1 / slides.length * 5) / 17 },
+//     { right: "25%", transform: "scale(1)", offset: (1 / slides.length * 6) / 17 },
+//     { right: "25%", transform: "scale(1)", offset: (1 / slides.length * 10) / 17 },
+//     { right: "75%", transform: "scale(.4)", offset: (1 / slides.length * 11) / 17 },
+//     { right: "75%", transform: "scale(.4)", offset: (1 / slides.length * 15) / 17 },
+//     { right: "150%", transform: "scale(.1)", offset: (1 / slides.length * 16) /17 },
+//     { right: "-50%", transform: "scale(.1)" }
+//   ], {
+//     duration: (5000 * (slides.length - 3)),
+//     iterations: Infinity,
+//     iterationStart,
+//     delay: (1 < idx && idx < (slides.length - 1)) ? ((idx - 1) * 1050) : 0 
+//   });
+// })
 
-  principal = entrada;
 
-  const entradaElm = slider.children.item(entrada);
-  entradaElm.ontransitionend = () => {
-    entradaElm.classList.add('principal');
-    entradaElm.classList.remove('entrada-principal', 'entrada');
-    entradaElm.ontransitionend = undefined;
-  }
-  entradaElm.classList.add('entrada-principal');
+// setInterval(() => {
+//   const salidaElm = slider.children.item(salida);
+//   salidaElm.ontransitionend = () => {
+//     salidaElm.classList.remove('salida', 'saliendo');
+//     console.count('salidaElm');
+//     salidaElm.ontransitionend = undefined;
+//   }
 
-  entrada = ((entrada + 1) < slides.length) ? entrada + 1 : 0;
+//   salidaElm.ontrasitioncancel = ()=> console.log('transition canceled SalidaElm')
+//   salidaElm.classList.add('saliendo');
 
-  const nuevoEntrada = slider.children.item(entrada);
-  nuevoEntrada.ontransitionend = () => {
-    nuevoEntrada.classList.add('entrada');
-    nuevoEntrada.classList.remove('entrando');
-    nuevoEntrada.ontransitionend = undefined;
-  }
-  nuevoEntrada.classList.add('entrando');
-}, 3000)
+//   salida = principal;
+//   const principalElm = slider.children.item(principal);
+//   principalElm.ontransitionend = () => {
+//     principalElm.classList.add('salida');
+//     principalElm.classList.remove('principal-salida', 'principal');
+//     console.count('principalElm');
+//     principalElm.ontransitionend = undefined;
+//   }
+//   salidaElm.ontrasitioncancel = ()=> console.log('transition canceled principalElm')
+//   principalElm.classList.add('principal-salida');
+
+//   principal = entrada;
+
+//   const entradaElm = slider.children.item(entrada);
+//   entradaElm.ontransitionend = () => {
+//     entradaElm.classList.add('principal');
+//     entradaElm.classList.remove('entrada-principal', 'entrada');
+//     console.count('entradaElm');
+//     entradaElm.ontransitionend = undefined;
+//   }
+
+//   salidaElm.ontrasitioncancel = ()=> console.log('transition canceled entradaElm')
+//   entradaElm.classList.add('entrada-principal');
+
+//   entrada = ((entrada + 1) < slides.length) ? entrada + 1 : 0;
+
+//   const nuevoEntrada = slider.children.item(entrada);
+//   nuevoEntrada.ontransitionend = () => {
+//     nuevoEntrada.classList.add('entrada');
+//     nuevoEntrada.classList.remove('entrando');
+//     console.count('nuevoEntrada');
+//     nuevoEntrada.ontransitionend = undefined;
+//   }
+
+//   salidaElm.ontrasitioncancel = ()=> console.log('transition canceled nuevoEntrada')
+//   nuevoEntrada.classList.add('entrando');
+// }, 3000)
 
 //_______________________BOTON SUBIR___________________________
 
